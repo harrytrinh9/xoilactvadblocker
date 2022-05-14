e9d95842edaa06c1252c75aa495eba7b497e017a
Hồng Hà Trịnh <trinhhongha90@hotmail.com.vn>
2022-05-14 16:40:20 +0700

    make some change

--- content.js
+++ content.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
-    removeQC();
+    // var topBanner = document.getElementsByClassName('container mt-1 d-none d-lg-block');
+
+    // console.log(topBanner);
+    // topBanner.remove();
+    jQuery('#overlay').prop('style', 'display: none;');
+    var bottom = document.getElementById('quangcaopc');
+    $(bottom).remove();
 })
 
 
 function removeQC(){
-    $('#overlay').remove();
-    $('#quangcaopc').remove();
-   
+    // jQuery('#overlay').prop('style', 'display: none;');
+    // jQuery('#quangcaopc').remove();
+    // var topBanner = $('.container .mt-1 .d-none .d-lg-block');
+    // var topBanner = document.getElementsByClassName('container mt-1 d-none d-lg-block');
+
+    // console.log(topBanner);
+    // topBanner.remove();
 }
